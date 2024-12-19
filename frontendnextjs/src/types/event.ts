04a4688968a1f588fa1b14b8b4cefa52eed9ed2c
tsaniafarihaa <tsaniafariha31@gmail.com>
2024-12-19 10:40:18 +0700

    commit pertama tsania

--- frontendnextjs/src/types/event.ts
+++ frontendnextjs/src/types/event.ts
@@ -3,10 +3,41 @@ export interface IUser {
   email: string;
   avatar: string;
   refCode: string;
-  percentage: number
+  percentage: number;
 }
 export interface IPromotor {
   name: string;
   email: string;
   avatar: string;
+}
+
+export interface Event {
+  id: number;
+  title: string;
+  slug: string;
+  thumbnail: string;
+  category: "Music" | "Orchestra" | "Opera" | "Other";
+  location: "Bandung" | "Bali" | "Surabaya" | "Jakarta";
+  venue: string;
+  description: string;
+  date: Date;
+  time: Date;
+  tickets: Ticket[];
+}
+
+export interface Ticket {
+  id: number;
+  category: string;
+  price: number;
+  quantity: number;
+  eventId?: number;
+  orderDetailId?: number;
+}
+
+export interface OrderDetail {
+  id: number;
+  quantity: number;
+  orderId?: number;
+  tickets: Ticket[];
+  userCouponId?: number;
 }
